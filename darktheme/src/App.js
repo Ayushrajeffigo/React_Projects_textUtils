13ca3ffe1a001dc7304531e4dd2380d70d439955
Ayush Raj <ayush.raj@effigo.tech>
2024-02-25 12:45:23 +0530

    enable mode features

---
+++ darktheme/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+          <Navbar/>
+          <About/>
+
+        </div>
+    </>
+  );
+}
+
+export default App;
